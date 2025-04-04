0943574aacb810e65890c28f98fad66b739f16f9
EduardoRomaniniPessoa <eduardo.pessoa@etec.sp.gov.br>
2025-04-03 22:28:34 -0300

    aula de revisao javascript

---
+++ aula-ja-01/numeros.js
@@ -0,0 +1,20 @@
+let n1 = 12;
+let n2 = 16;
+
+console.log(n1);
+console.log(n1,n2);
+console.log(n1 * n2);
+let soma = n1 + n2
+console.log(`${n1} + ${n2} = ${soma}`)
+
+let subtracao = n1 - n2
+console.log(`${n1} - ${n2} = ${subtracao}`)
+
+let multiplicacao = n1 * n2
+console.log(`${n1} * ${n2} = ${multiplicacao}`)
+
+let divisao = n1 / n2
+console.log(`${n1} / ${n2} = ${divisao}`)
+
+let potencia = Math.pow(n1,n2)
+console.log(`${n1} ^ ${n2} = ${potencia}`)
---
+++ aula-ja-01/ola.js
@@ -0,0 +1,10 @@
+console.log("Olá mundo!");
+
+var msg = "quero chegar na pessoa de cabelo rosa";
+console.log(msg);
+
+let resposta = "mas tenho medo";
+console.log(resposta);
+
+const turma = "to com medo de viver";
+console.log(turma);
